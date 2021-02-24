47043119a1645ec2fd1b910fe963948a33065473
Josh Dawson <joshuadawson123@googlemail.com>
2021-02-24 15:10:54 +0000

    Added ability to auto change immunity after given time period

--- contracts/Passport.sol
+++ contracts/Passport.sol
@@ -4,12 +4,12 @@ pragma solidity ^0.7.4;
 contract Passport {
     address administrator; // address of who deployed the contract (admin access for certain operations)
     mapping(address => bool) verifiedUsers; // mapping of verified users
-    mapping(bytes32 => Citizen) public passport; // map hashes to citizens (wont be public in final implementation)
+    mapping(bytes32 => Citizen) public passports; // map hashes to citizens (wont be public in final implementation)
     mapping(uint256 => bytes32) public ids;
     uint256 public passportCount = 0;
+    uint256 public lastAutoUpdate = 0;
 
     struct Citizen {
-        bytes32 hashID;
         string subdivisionCode; // store county code of citizen eg (GB-NBL = Northumberland)
         uint8 age;
         bool immunity; // a bool to represent immunity (true = immune)
@@ -19,6 +19,7 @@ contract Passport {
     constructor() {
         administrator = msg.sender;
         verifiedUsers[msg.sender] = true;
+        lastAutoUpdate = block.timestamp;
     }
 
     modifier admin() {
@@ -61,22 +62,15 @@ contract Passport {
         Citizen memory newCitizen;
         if (_immunityStatus) {
             newCitizen = Citizen(
-                _hash,
                 _subdivisionCode,
                 _age,
                 _immunityStatus,
                 block.timestamp
             );
         } else {
-            newCitizen = Citizen(
-                _hash,
-                _subdivisionCode,
-                _age,
-                _immunityStatus,
-                0
-            );
+            newCitizen = Citizen(_subdivisionCode, _age, _immunityStatus, 0);
         }
-        passport[_hash] = newCitizen;
+        passports[_hash] = newCitizen;
 
         passportCount++;
         ids[passportCount] = _hash;
@@ -87,20 +81,120 @@ contract Passport {
     }
 
     function isImmune(bytes32 _hash) public view returns (bool) {
-        return passport[_hash].immunity;
+        return passports[_hash].immunity;
     }
 
     function updateImmunity(bytes32 _hash, bool _immunityStatus)
         public
         verified
     {
-        passport[_hash].immunity = _immunityStatus;
+        if (_immunityStatus) {
+            passports[_hash].immunity = _immunityStatus;
+            passports[_hash].immuneTime = block.timestamp;
+        } else {
+            passports[_hash].immunity = _immunityStatus;
+            passports[_hash].immuneTime = 0;
+        }
+    }
+
+    function autoUpdateImmunity() public verified {
+        if (lastAutoUpdate + 5 < block.timestamp) {
+            for (uint256 i = 1; i < passportCount; i++) {
+                if (passports[ids[i]].immunity == true) {
+                    passports[ids[i]].immunity = false;
+                    passports[ids[i]].immuneTime = 0;
+                }
+            }
+        }
     }
 
     event Transfer(address indexed _address, uint256 _amount);
     event VerifiedUser(address indexed _address);
     event UnverifiedUser(address indexed _address);
     event ImmuneCase(string _subdivisionCode, uint8 _age);
+
+    function addTestingCitizens() public admin {
+        // temporary testing function to populate citizens
+        passports[
+            0x3ac225168df54212a25c1c01fd35bebfea408fdac2e31ddd6f80a4bbf9a5f1cb
+        ] = Citizen("ENG-AAA", 1, true, block.timestamp);
+        ids[
+            1
+        ] = 0x3ac225168df54212a25c1c01fd35bebfea408fdac2e31ddd6f80a4bbf9a5f1cb;
+        passportCount++;
+
+        passports[
+            0x67fad3bfa1e0321bd021ca805ce14876e50acac8ca8532eda8cbf924da565160
+        ] = Citizen("ENG-BBB", 2, false, 0);
+        ids[
+            2
+        ] = 0x67fad3bfa1e0321bd021ca805ce14876e50acac8ca8532eda8cbf924da565160;
+        passportCount++;
+
+        passports[
+            0x4e03657aea45a94fc7d47ba826c8d667c0d1e6e33a64a036ec44f58fa12d6c45
+        ] = Citizen("ENG-CCC", 3, true, block.timestamp);
+        ids[
+            3
+        ] = 0x4e03657aea45a94fc7d47ba826c8d667c0d1e6e33a64a036ec44f58fa12d6c45;
+        passportCount++;
+
+        passports[
+            0x48bed44d1bcd124a28c27f343a817e5f5243190d3c52bf347daf876de1dbbf77
+        ] = Citizen("ENG-DDD", 4, true, block.timestamp);
+        ids[
+            4
+        ] = 0x48bed44d1bcd124a28c27f343a817e5f5243190d3c52bf347daf876de1dbbf77;
+        passportCount++;
+
+        passports[
+            0x6377c7e66081cb65e473c1b95db5195a27d04a7108b468890224bedbe1a8a6eb
+        ] = Citizen("ENG-EEE", 5, false, 0);
+        ids[
+            5
+        ] = 0x6377c7e66081cb65e473c1b95db5195a27d04a7108b468890224bedbe1a8a6eb;
+        passportCount++;
+
+        passports[
+            0xacd0c377fe36d5b209125185bc3ac41155ed1bf7103ef9f0c2aff4320460b6df
+        ] = Citizen("ENG-FFF", 6, true, block.timestamp);
+        ids[
+            6
+        ] = 0xacd0c377fe36d5b209125185bc3ac41155ed1bf7103ef9f0c2aff4320460b6df;
+        passportCount++;
+
+        passports[
+            0xa82aec019867b7307551dc397acde18b541e742fa1a4e53df4ce3b02d462f524
+        ] = Citizen("ENG-GGG", 7, false, 0);
+        ids[
+            7
+        ] = 0xa82aec019867b7307551dc397acde18b541e742fa1a4e53df4ce3b02d462f524;
+        passportCount++;
+
+        passports[
+            0x48624fa43c68d5c552855a4e2919e74645f683f5384f72b5b051b71ea41d4f2d
+        ] = Citizen("ENG-HHH", 8, false, 0);
+        ids[
+            8
+        ] = 0x48624fa43c68d5c552855a4e2919e74645f683f5384f72b5b051b71ea41d4f2d;
+        passportCount++;
+
+        passports[
+            0x34fb2702da7001bf4dbf26a1e4cf31044bd95b85e1017596ee2d23aedc90498b
+        ] = Citizen("ENG-III", 9, true, block.timestamp);
+        ids[
+            9
+        ] = 0x34fb2702da7001bf4dbf26a1e4cf31044bd95b85e1017596ee2d23aedc90498b;
+        passportCount++;
+
+        passports[
+            0xf8da54b5a7dd75028acb077ee61e8dde47ed37c746703ce764edf4a789eb2103
+        ] = Citizen("ENG-JJJ", 5, false, 0);
+        ids[
+            10
+        ] = 0xf8da54b5a7dd75028acb077ee61e8dde47ed37c746703ce764edf4a789eb2103;
+        passportCount++;
+    }
 }
 
 // TODO: Add timed function which changes immunity status after 14 days (from addition to the blockchain)
