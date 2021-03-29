1b5e04568321a752289174995859ebb26db9ad77
Josh Dawson <joshuadawson123@googlemail.com>
2021-03-29 14:39:06 +0100

    added test for removing verified users

--- test/PassportTesting.js
+++ test/PassportTesting.js
@@ -1,3 +1,5 @@
+const { assert } = require("chai");
+
 var Passport = artifacts.require("./Passport.sol");
 
 contract("Passport", function (accounts) {
@@ -15,23 +17,56 @@ contract("Passport", function (accounts) {
   });
 
   it("verified user added succesfully", function () {
+    let address = "0xFDEFF5a015b517fdd67e2A00FadD138De3bA6Cf6";
     return Passport.deployed()
       .then(function (instance) {
         passportInstance = instance;
-        return instance.isVerified("0xFDEFF5a015b517fdd67e2A00FadD138De3bA6Cf6");
+        return instance.isVerified(address);
       })
       .then(function (verifiedStatus) {
         assert.isFalse(verifiedStatus);
         passportInstance
-          .addVerifiedUser("0xFDEFF5a015b517fdd67e2A00FadD138De3bA6Cf6")
+          .addVerifiedUser(address)
           .then(function (addVerified) {
-            return passportInstance.addVerifiedUser("0xFDEFF5a015b517fdd67e2A00FadD138De3bA6Cf6");
+            return passportInstance.addVerifiedUser(address);
           })
           .then(function (changeStatus) {
-            return passportInstance.isVerified("0xFDEFF5a015b517fdd67e2A00FadD138De3bA6Cf6");
+            return passportInstance.isVerified(address);
           })
           .then(function (newVerifiedStauts) {
             assert.isTrue(newVerifiedStauts);
+          });
+      });
+  });
+
+  it("verified user removed succesfully", function () {
+    let address = "0x79e7264b03064d7CEB764f407D7a607Cb1b3397e";
+    return Passport.deployed()
+      .then(function (instance) {
+        passportInstance = instance;
+        return instance.isVerified(address);
+      })
+      .then(function (verifiedStatus) {
+        assert.isFalse(verifiedStatus);
+        passportInstance
+          .addVerifiedUser(address)
+          .then(function (addVerified) {
+            return passportInstance.addVerifiedUser(address);
+          })
+          .then(function (changeStatus) {
+            return passportInstance.isVerified(address);
+          })
+          .then(function (newVerifiedStauts) {
+            assert.isTrue(newVerifiedStauts);
+          })
+          .then(function (removeVerified) {
+            return passportInstance.removeVerified(address);
+          })
+          .then(function (removedStatus) {
+            return passportInstance.isVerified(address);
+          })
+          .then(function (checkVerified) {
+            assert.isFalse(checkVerified);
           });
       });
   });
